38f281c55bdce1162be8872f7227eac056138ebc
Acellama88 <acellama88@gmail.com>
2023-12-23 11:40:13 -0600

    New Repository

---
+++ Day 01/Day01_p2.py
@@ -0,0 +1,28 @@
+import re
+
+file = "./"+ __file__.split('\\')[-1].split('.')[0]+"/input.txt"
+finalTotal = 0
+input = []
+
+def parse():
+    with open(file) as f:
+        for line in f:
+            if False:
+                input.extend(re.split("",line.strip()))
+            elif False:
+                input.append(int(line.strip()))
+            else:
+                input.append(line.strip())
+
+def part1():
+    
+    print(0)
+
+def part2():
+    
+    print(0)
+
+if __name__ == '__main__':
+    parse()
+    part1()
+    part2()
